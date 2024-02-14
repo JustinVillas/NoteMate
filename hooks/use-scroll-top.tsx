900eaf34de441a22947fef63bfe1ad50d2dd3d48
JustinVillas <92616184+JustinVillas@users.noreply.github.com>
2024-02-15 03:13:38 +0800

    fix error

--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const useScrollTop = (threshold = 10) => {
   const [scrolled, setScrolled] = useState(false);
